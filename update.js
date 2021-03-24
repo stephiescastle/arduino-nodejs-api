1e251d400e9831c7e4a842a0e9fca228fa87e4f2
stephiescastle <stephiesmith@gmail.com>
2021-03-24 00:09:31 -0700

    using serial port instead

--- update.js
+++ update.js
@@ -9,12 +9,50 @@ const pin = {
   value: 1200,
 };
 
+const allPins = [
+  {
+    id: "A0",
+    value: 10,
+  },
+  {
+    id: "A1",
+    value: 356,
+  },
+  {
+    id: "A2",
+    value: 487,
+  },
+];
+
+for (const pin of allPins) {
+  // const user = await fetch(`https://remote-server.com/user/${id}.json`);
+  // users.push(user);
+  console.log(`${process.env.API_HOST}/pins/${pin.id}`);
+  fetch(`${process.env.API_HOST}/pins/${pin.id}`, {
+    method: "PUT",
+    body: JSON.stringify(pin),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+}
 // updates pin A0
-fetch(`${process.env.API_HOST}/pins/A0`, {
-  method: "PUT",
-  body: JSON.stringify(pin),
-  headers: { "Content-Type": "application/json" },
-})
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.log(err));
+// fetch(`${process.env.API_HOST}/pins/A0`, {
+//   method: "PUT",
+//   body: JSON.stringify(pin),
+//   headers: { "Content-Type": "application/json" },
+// })
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.log(err));
+
+// // update multiple pins
+// fetch(`${process.env.API_HOST}/pins/A1`, {
+//   method: "PUT",
+//   body: JSON.stringify(allPins[1]),
+//   headers: { "Content-Type": "application/json" },
+// })
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.log(err));

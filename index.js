16f64331486107522b3ef865c0ee06e3f52a2dcb
stephiescastle <stephiesmith@gmail.com>
2021-04-01 19:54:35 -0700

    clean up

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+// import dependencies
 require("dotenv").config();
 const fetch = require("node-fetch");
 const SerialPort = require("serialport");
 const Readline = require("@serialport/parser-readline");
 
-const interval = 500; // *️⃣ time interval for POST requests
-let pins = []; // empty array to store pin data
+// vars *️⃣ update to match your configuration
+const portName = "/dev/cu.SLAB_USBtoUART"; // the name of the serial port (same as arduino port)
+const baudRate = 9600; // should match the arduino's baudrate
+const interval = 500; // time interval for POST requests
 
-// setup our serial port
-const port = new SerialPort(
-  "/dev/cu.SLAB_USBtoUART", // *️⃣ the name of the serial port you want to open (same as arduino port)
-  { baudRate: 9600 }, // *️⃣ be sure to match the arduino's baudrate
-  function (err) {
-    if (err) {
-      console.log("Error: ", err.message);
-    }
+// an empty array to store pin data
+let pins = [];
+
+// setup the serial port
+const port = new SerialPort(portName, { baudRate: baudRate }, function (err) {
+  if (err) {
+    console.log("Error: ", err.message);
   }
-);
+});
 
-// setup our parser to read the serial port
+// setup a parser to read the serial port
 const parser = port.pipe(new Readline({ delimiter: "\n" })); // Read the port data
 
 // do stuff once the port is open
 port.on("open", () => {
-  console.log("serial port open");
+  console.log("✅ serial port open");
 
-  // parse received data
+  // parse data
   parser.on("data", (data) => {
     // remove any trailing whitespace and \n
     data = data.trim();
-    // split at delimiter and put into the pins array
-    pins = data.split("\t");
+    // split at delimiter, convert to number, and assign to pins array
+    pins = data.split("\t").map(Number);
   });
 
   // send our data to the net
@@ -63,10 +65,4 @@ port.on("open", () => {
         .catch((err) => console.log(err));
     }
   }, interval);
-});
-
-port.on("close", () => {
-  console.log("serial port closed");
-  // stop sending data
-  clearInterval(sendData);
 });
